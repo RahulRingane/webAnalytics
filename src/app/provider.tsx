77db1eb4200117f99d655e3d75f8a4d8e493e3dc
Rahul Ringane <rahul897r@gmail.com>
2025-07-19 23:35:20 +0530

    fix: auth error

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
+import { headers } from "next/headers";
 type Props = {
   children: React.ReactNode;
 };
 
 const Provider = async ({ children }: Props) => {
+
+  await headers();
+
   const session = await auth();
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,4 @@
-import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/db";
-import Google from "next-auth/providers/google";
+
 import type { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
@@ -11,32 +8,52 @@ declare module "next-auth" {
     };
   }
 }
+import NextAuth from "next-auth";
+import { authConfig } from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "@/lib/db";
+import  Google   from 'next-auth/providers/google'
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  ...authConfig,
+  adapter: PrismaAdapter(db),
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID || "",
       clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/signin",
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
+        // get user from db with the email
+        // if there is no user with the email, create new user
+        // else set the user data to token
         token.id = user.id;
       }
       return token;
     },
+
     async session({ session, token }) {
       if (token) {
+        // set the token data to session
         if (session.user) {
           session.user.id = token.id as string;
         }
       }
+
       return session;
     },
-  },
-  session: {
-    strategy: "jwt",
+
+    redirect() {
+      return "/signin";
+    },
   },
 });
