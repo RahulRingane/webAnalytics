c7879aeebaf8002b144a1c16b226803621f29d78
Rahul Ringane <rahul897r@gmail.com>
2025-07-26 13:45:38 +0530

    fix: metadata

--- src/app/(root)/projects/action.ts
+++ src/app/(root)/projects/action.ts
@@ -6,7 +6,11 @@ import { extractMetadata } from "@/lib/metadata";
 export async function fetchMetadataAction(domain: string) {
   try {
     const data = await extractMetadata(`https://${domain}`);
-    return data;
+    if (data.error) {
+      console.log("Error fetching metadata:", data.error);
+      return null;
+    }
+    return data.data;
   } catch (error) {
     console.error("Error fetching metadata:", error);
     return null;
--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,12 +1,13 @@
 // src/lib/metadata.ts
+import { error } from "console";
 import getMetaData from "metadata-scraper";
 
 export async function extractMetadata(url: string) {
   try {
     const data = await getMetaData(url);
-    return data;
+    return {data, error: null};
   } catch (error) {
     console.error("Error extracting metadata:", error);
-    return null;
+    return {data: null, error};
   }
 }
