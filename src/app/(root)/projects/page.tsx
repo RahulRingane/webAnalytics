d4963c49e72337ab930c31ebb43fa36fc3a3d354
Rahul Ringane <rahul897r@gmail.com>
2025-07-27 00:28:17 +0530

    chore: add project skeleton

--- src/app/(root)/projects/page.tsx
+++ src/app/(root)/projects/page.tsx
@@ -7,38 +7,36 @@ import { EditModal } from "./_components/modal/edit";
 import { ProjectCard } from "./_components/project-card";
 import { getAllProjects } from "@/use-cases/project";
 import { Suspense } from "react";
+import { ProjectSkelteon } from "./_components/project-skeleton";
 
 const ProjectsPage = async () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Projects />
-    </Suspense>
+    <div className="flex flex-col w-full h-full">
+      <Header />
+      <Suspense fallback={<ProjectSkelteon />}>
+        <Projects />
+      </Suspense>
+      <CreateModal />
+      <EditModal />
+      <DeleteModal />
+    </div>
   );
 };
 
 const Projects = async () => {
   const session = await assertAuthenticated();
   const projects = await getAllProjects(session.id);
-  console.log(projects, "projectsssss")
-  return (
-    <div className="flex flex-col w-full h-full">
-      <Header />
-      {projects && Array.isArray(projects) && projects.length > 0 ? (
-        <div className="p-3 w-full h-full">
-          <div className="gap-6 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 w-full">
-            {projects.map((data, index) => (
-              <ProjectCard key={index} data={data} />
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div className="flex flex-1 justify-center items-center w-full h-full">
-          <EmptyProject />
-        </div>
-      )}
-      <CreateModal />
-      <EditModal />
-      <DeleteModal />
+  return projects && Array.isArray(projects) && projects.length > 0 ? (
+    <div className="p-3 w-full h-full">
+      <div className="gap-6 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 w-full">
+        {projects.map((data, index) => (
+          <ProjectCard key={index} data={data} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex flex-1 justify-center items-center w-full h-full">
+      <EmptyProject />
     </div>
   );
 };
