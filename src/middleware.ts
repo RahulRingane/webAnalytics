07a0d4d08530faf61780263b7f19fec70001b355
Rahul Ringane <rahul897r@gmail.com>
2025-07-27 15:11:32 +0530

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,11 @@ export default auth((req: any) => {
 
   const isAuthenticated = !!req.auth;
 
+  if (nextUrl.pathname === "/api/track" || nextUrl.pathname === "/tracking-script.js") {
+   console.log("Passed")
+   return NextResponse.next();
+ }
+ 
   console.log("isAuthenticated", isAuthenticated);
 
   const isAuthRoute = AUTH_ROUTES.includes(nextUrl.pathname);
